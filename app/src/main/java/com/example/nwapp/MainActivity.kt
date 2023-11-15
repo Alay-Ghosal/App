8d68f32a6f0a6848fa556cd32426c6cf87ed0599
Utsav Chatterjee <c.utsav1@gmail.com>
2023-11-15 09:01:35 +0530

    Created new CenterAlignedText function to compose text in the center of the screen

--- app/src/main/java/com/example/nwapp/MainActivity.kt
+++ app/src/main/java/com/example/nwapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.nwapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.nwapp.ui.theme.NWAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +26,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CenterAlignedText("Alay")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun CenterAlignedText(name: String) {
+    // https://developer.android.com/jetpack/compose/documentation
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.SemiBold
+        )
     }
 }
 
